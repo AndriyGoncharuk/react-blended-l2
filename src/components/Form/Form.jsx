6a32da62338d12346d6a4bededbde825082cc58c
AndriyGoncharuk <160170957+AndriyGoncharuk@users.noreply.github.com>
2024-08-24 15:55:39 +0300

    hw1

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,37 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ onSubmit }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!searchQuery.trim()) {
+      return alert('cannot be empty!');
+    }
+    onSubmit(searchQuery);
+    setSearchQuery('');
+  };
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        value={searchQuery}
+        onChange={handleChange}
+        required
+        autoFocus
+      />
+    </form>
+  );
 };
--- src/tabs/Photos.jsx
+++ src/tabs/Photos.jsx
@@ -1,9 +1,42 @@
-// import { getPhotos } from 'apiService/photos';
-import { Text } from 'components';
+import { getPhotos } from 'apiService/photos';
+import { Form, Text } from 'components';
+import { useEffect, useState } from 'react';
 
 export const Photos = () => {
+  const [query, setQuery] = useState('');
+
+  const [page, setPage] = useState(1);
+
+  const [images, setImages] = useState([]);
+
+  const [isloading, setIsloading] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    const fetchData = async () => {
+      setIsloading(true);
+      try {
+        const data = await getPhotos(query, page);
+        console.log(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsloading(false);
+      }
+    };
+    fetchData();
+  }, [page, query]);
+
+  const onHandleSubmit = searchQuery => {
+    setQuery(searchQuery);
+  };
   return (
     <>
+      <Form onSubmit={onHandleSubmit} />
       <Text textAlign="center">Let`s begin search 🔎</Text>
     </>
   );
